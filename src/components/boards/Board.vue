11aba912ef93f9c35291047be79ed6a247cebdc4
Uzi Landsmann <uzi.landsmann@gmail.com>
2025-04-12 10:39:11 +0200

    Added dealer marker

--- src/components/boards/Board.vue
+++ src/components/boards/Board.vue
@@ -16,15 +16,22 @@
     <CTableBody>
       <CTableRow v-for="(player, playerIndex) in playersStore.players" :key="playerIndex">
         <CTableDataCell v-model="player.name">
-          <CFormInput
-            type="text"
-            id="playerName"
-            label=""
-            placeholder=""
-            v-model="player.name"
-            key="playerIndex"
-            >{{ player.name }}
-          </CFormInput>
+          <div class="dealer">
+            <div v-if="dealerNumber === playerIndex" class="dealer-marker">
+              <CIcon :icon="cilChevronRight" size="xl" class="dealer-icon" />
+            </div>
+            &nbsp;
+            <div v-else class="dealer-space">&nbsp;</div>
+            <CFormInput
+              type="text"
+              id="playerName"
+              label=""
+              placeholder=""
+              v-model="player.name"
+              key="playerIndex"
+              >{{ player.name }}
+            </CFormInput>
+          </div>
         </CTableDataCell>
         <CTableDataCell v-for="(_, pointIndex) in player.points" v-bind:key="pointIndex">
           <CFormInput
@@ -47,7 +54,6 @@
   </div>
 </template>
 <script setup lang="ts">
-import { cilCircle, cilMinus } from '@coreui/icons'
 import {
   CTable,
   CTableBody,
@@ -57,12 +63,13 @@ import {
   CTableRow
 } from '@coreui/vue'
 import { CFormInput } from '@coreui/vue/dist/esm/components/form'
-import { CIcon } from '@coreui/icons-vue'
 import type { Player } from '@/models/Player'
 import { CButton } from '@coreui/vue/dist/esm/components/button'
 import { ref, watch } from 'vue'
 import { usePlayersStore } from '@/stores/PlayersStore'
 import CellWithIcons from '@/components/boards/CellWithIcons.vue'
+import CIcon from '@coreui/icons-vue'
+import { cilChevronRight } from '@coreui/icons'
 
 const playersStore = usePlayersStore()
 
@@ -72,12 +79,25 @@ const sum = (player: Player): number => {
 
 const lastGamePlayed = ref<boolean>(false)
 
+const dealerNumber = ref(0)
+
 watch(
   playersStore.players,
   () => {
     const lasts = playersStore.players.map((player) => player.points[6])
     const registeredLasts = lasts.filter((points) => points > 0)
+    const isNullEmptyOrZero = (x: number) => x === null || x === 0 || String(x) === ''
     lastGamePlayed.value = registeredLasts.length > 0
+
+    const games = []
+    for (let i = 0; i < 7; i++) {
+      const map = playersStore.players.map((player) => player.points[i])
+      const items = map.some((x) => !isNullEmptyOrZero(x))
+      games.push(items)
+    }
+
+    const currentGame = games.lastIndexOf(true) + 1
+    dealerNumber.value = currentGame % playersStore.players.length
   },
   {
     deep: true,
@@ -104,5 +124,24 @@ td {
 
 .sum {
   font-weight: bold;
+}
+
+.dealer {
+  display: flex;
+  align-items: center;
+}
+
+.dealer-icon {
+  color: #ffffff;
+  padding-left: 0;
+  margin-left: -5px;
+}
+
+.dealer-space {
+  width: 22px;
+}
+
+.dealer-marker {
+  display: flex;
 }
 </style>
