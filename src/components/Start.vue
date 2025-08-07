fd510aa1248f4814da6d5545dbf56ec014df851c
Uzi Landsmann <uzi.landsmann@gmail.com>
2025-08-07 22:29:32 +0200

    Move tab buttons to radio buttons at top of page between restart and statistics buttons

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -12,7 +12,42 @@
       <Players />
     </div>
     <div v-if="playersStore.players.length > 0" class="game" key="players.length">
-      <Boards @saveGame="saveGame" />
+      <div class="tab-buttons">
+        <div class="radio-group">
+          <CFormCheck
+            type="radio"
+            name="boardType"
+            :value="'normal-board'"
+            :checked="activeTab === 'normal-board'"
+            @change="activeTab = 'normal-board'"
+            class="tab-radio"
+          />
+          <span class="radio-label">{{ $t('message.ubb') }}</span>
+        </div>
+        <div class="radio-group">
+          <CFormCheck
+            type="radio"
+            name="boardType"
+            :value="'long-board'"
+            :checked="activeTab === 'long-board'"
+            @change="activeTab = 'long-board'"
+            class="tab-radio"
+          />
+          <span class="radio-label">{{ $t('message.longUbb') }}</span>
+        </div>
+        <div class="radio-group">
+          <CFormCheck
+            type="radio"
+            name="boardType"
+            :value="'longer-board'"
+            :checked="activeTab === 'longer-board'"
+            @change="activeTab = 'longer-board'"
+            class="tab-radio"
+          />
+          <span class="radio-label">{{ $t('message.longerUbb') }}</span>
+        </div>
+      </div>
+      <Boards :activeTab="activeTab" @saveGame="saveGame" />
     </div>
   </template>
 
@@ -34,8 +69,9 @@ import { useGamesStore } from '@/stores/GamesStore'
 import Players from '@/components/Players.vue'
 import Boards from '@/components/boards/Boards.vue'
 import GameSaved from '@/components/GameSaved.vue'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
 import type { User } from '@firebase/auth'
+import { CFormCheck } from '@coreui/vue/dist/esm/components/form'
 // import { firebaseConfig } from '@/credentials-dev'
 
 // Component name for linting
@@ -46,6 +82,7 @@ defineOptions({
 const authStore = useAuthStore()
 const playersStore = usePlayersStore()
 const gamesStore = useGamesStore()
+const activeTab = ref('normal-board')
 
 const saveGame = () => {
   gamesStore.saveGame(playersStore.game(firebase?.auth()?.currentUser?.displayName ?? 'N/A'))
@@ -93,6 +130,32 @@ th,
   padding-top: 10px;
   display: grid;
   width: 100%;
+}
+
+.tab-buttons {
+  position: absolute;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  display: flex;
+  gap: 10px;
+}
+
+.radio-group {
+  display: flex;
+  align-items: center;
+  margin-right: 20px;
+}
+
+.tab-radio {
+  margin-right: 0;
+}
+
+.radio-label {
+  color: white;
+  font-weight: normal;
+  font-size: 14px;
+  margin-left: 8px;
 }
 
 /* Chrome, Safari, Edge, Opera */
--- src/components/boards/Boards.vue
+++ src/components/boards/Boards.vue
@@ -1,29 +1,19 @@
 <template>
-  <CTabs activeItemKey="normal-board">
-    <CTabList variant="pills">
-      <CTab itemKey="normal-board">{{ $t('message.ubb') }}</CTab>
-      <CTab itemKey="long-board">{{ $t('message.longUbb') }}</CTab>
-      <CTab itemKey="longer-board">{{ $t('message.longerUbb') }}</CTab>
-    </CTabList>
-    <CTabContent class="scroll">
-      <CTabPanel class="p-3" itemKey="normal-board">
-        <Board @saveGame="emit('saveGame')" />
-      </CTabPanel>
-      <CTabPanel class="p-3" itemKey="long-board">
-        <LongBoard />
-      </CTabPanel>
-      <CTabPanel class="p-3" itemKey="longer-board">
-        <LongerBoard />
-      </CTabPanel>
-    </CTabContent>
-  </CTabs>
+  <div class="board-content">
+    <Board v-if="props.activeTab === 'normal-board'" @saveGame="emit('saveGame')" />
+    <LongBoard v-else-if="props.activeTab === 'long-board'" />
+    <LongerBoard v-else-if="props.activeTab === 'longer-board'" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { CTab, CTabContent, CTabList, CTabPanel, CTabs } from '@coreui/vue/dist/esm/components/tabs'
 import Board from '@/components/boards/Board.vue'
 import LongBoard from '@/components/boards/LongBoard.vue'
 import LongerBoard from '@/components/boards/LongerBoard.vue'
+
+const props = defineProps<{
+  activeTab: string
+}>()
 
 const emit = defineEmits(['saveGame'])
 </script>
