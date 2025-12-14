498a46dd22ff0a458962bad6b82c7e712c9e924d
Uzi Landsmann <uzi.landsmann@gmail.com>
2025-12-14 07:04:31 +0100

    bumped versions

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,16 @@ import tseslint from 'typescript-eslint'
 import pluginVue from 'eslint-plugin-vue'
 
 export default [
+  { ignores: ['dist', 'node_modules', '.idea', '.vscode'] },
   { files: ['**/*.{js,mjs,cjs,ts,vue}'] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...pluginVue.configs['flat/essential'],
-  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } }
+  { files: ['**/*.vue'], languageOptions: { parserOptions: { parser: tseslint.parser } } },
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
+  }
 ]
